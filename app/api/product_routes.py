6b622e8c9ba220e491c618a99d1dbfe4e8de74be
Katy Kam <katyky14@gmail.com>
2022-10-14 18:29:54 -0700

    product routes

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, redirect
 from ..forms.product_form import ProductForm,EditProductForm
-from ..models import Product, Review, Image
+from ..models import Product, Review, Image, db
 
 
 product_routes = Blueprint('products', __name__)
@@ -17,42 +17,53 @@ def get_all_products():
 #Get product by id
 @product_routes.route('/<int:id>')
 def get_product_id(id):
-#    user = User.query.get(id)
-#     return user.to_dict()
-    pass
+    one_product = Product.query.get(id)
+    # print("product", one_product)
+    return {'oneProduct': one_product.to_dict_relationship()}
+
 
 
 #Get product by the current owner
 
 #Add a product
-@product_routes.route('/',methods=["POST"])
+@product_routes.route('',methods=["POST"])
 def post_all_products():
-    pass
-    # form = ProductForm()
-    # if form.validate_on_submit():
-    #     # data =
-    #     form.populate_obj(data):
-    #     db.session.add(data)
-    #     db.session.commit()
-    # return "ADD FORM"
+
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        data = Product()
+        form.populate_obj(data)
+        db.session.add(data)
+        db.session.commit()
+        return "Added product"
+
+    return "ERRORS"
+
 
 
 #Edit a product listing
 @product_routes.route("/<int:id>",methods=["PUT"])
-def update_product():
-     # form = EditProductForm()
-    # if form.validate_on_submit():
-    #     # data =
-    #     form.populate_obj(data):
-    #     db.session.add(data)
-    #     db.session.commit()
-    pass
+def update_product(id):
+    form = EditProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        data = Product.query.get(id)
+        form.populate_obj(data)
+        db.session.add(data)
+        db.session.commit()
+        return "Edited a product"
+
+    return "error"
 
 
 #Delete a product
 @product_routes.route("/<int:id>",methods=["DELETE"])
-def delete_product():
-    pass
+def delete_product(id):
+    selected_product = Product.query.get(id)
+    db.session.delete(selected_product)
+    db.session.commit()
+    return "Deleted Successfully"
 
 
 #Get comments for product
