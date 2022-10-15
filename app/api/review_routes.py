aa84234aa7fb0bfbecb290b20e708bca90a99d44
TonyTan <tonytan2193@gmail.com>
2022-10-15 15:19:05 -0700

    Added some more seeders to reviews

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,8 @@
 from crypt import methods
 from flask import Blueprint, jsonify
 from ..forms.reviews_form import ReviewForm,EditReviewForm
+from flask_login import current_user
+from ..models import Review, db
 
 
 review_routes = Blueprint('reviews', __name__)
@@ -9,21 +11,23 @@ review_routes = Blueprint('reviews', __name__)
 #Get comments for users
 @review_routes.route('/user')
 def get_all_comments_user():
-    # allProducts = Product.query.all()
+    # NEED TO FIGURE OUT HOW TO GET CURRENT USER ID
+    # all_reviews = Review.query.filter(Review.user_id == #CURRENT user_id HERE!!!)
+    # return {"Reviews": [review.to_dict_reviews() for review in all_reviews]}
     pass
 
 #Add comment for product
 @review_routes.route('/',methods=["POST"])
 def add_comment():
-    pass
-    # form = ReviewForm()
-    # if form.validate_on_submit():
-    #     # data =
-    #     form.populate_obj(data):
-    #     db.session.add(data)
-    #     db.session.commit()
-    # return "ADD FORM"
-    pass
+    form = ReviewForm()
+    if form.validate_on_submit():
+        data = Review()
+        form.populate_obj(data)
+        db.session.add(data)
+        db.session.commit()
+        return "Review added to product"
+    return "Error"
+
 
 #Edit comment for product
 @review_routes.route('/<int:review_id>',methods=["PUT"])
