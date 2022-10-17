034fc4ba489c78a40ff17c805c003bd027a10b1c
TonyTan <tonytan2193@gmail.com>
2022-10-17 15:18:24 -0700

    Made product display and redirect to product details in homepage

--- react-app/src/components/products/getProducts.js
+++ react-app/src/components/products/getProducts.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { getAllProductsThunk } from "../../store/product";
 import "./homepage.css"
 
 
 function Products() {
     const dispatch = useDispatch()
+    const history = useHistory();
     const productObj = useSelector(state => state.productState)
     const productArr = Object.values(productObj)
 
@@ -15,17 +17,14 @@ function Products() {
     }, [dispatch])
 
     return !!productArr.length && (
-        <div>
-            <h1>Products</h1>
-            <div id="homepage">
-                <div>
-                    {productArr.map(({ id, price, images }) => (
-                        <div key={id}>
-                            <img style={{ width: 300, height: 500 }} src={images.map(image => image.mainImage ? image.image_url : null)} alt={"Preview Spot"} />
-                            <div>${price.toFixed(2)}</div>
-                        </div>
-                    ))}
-                </div>
+        <div id="homepage">
+            <div id="product-container">
+                {productArr.map(({ id, name, price, images }) => (
+                    <div key={id} onClick={() => history.push(`/products/${id}`)} className="homepage-products">
+                        <img id="previewImage" src={images.map(image => image.mainImage ? image.image_url : null)} alt={name} />
+                        <div id="product-price">${price.toFixed(2)}</div>
+                    </div>
+                ))}
             </div>
         </div>
     )
