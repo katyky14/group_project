79c3aaed92f85eb67aab19b248f8409f39678b80
kevin <kmtsou@sbcglobal.net>
2022-10-18 08:52:33 -0700

    profile button dropdwon

--- react-app/src/components/NavBar/ProfileButton.js
+++ react-app/src/components/NavBar/ProfileButton.js
@@ -1,3 +1,58 @@
 import { useState, useEffect } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { logout } from '../../store/session';
+
+function ProfileButton({sessionUser}) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [showMenu, setShowMenu] = useState(false);
+
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu])
+
+    const loggingOut = async (e) => {
+        e.preventDefault();
+        let loggedOut = await dispatch(logout());
+        if (loggedOut) {
+            history.push('/')
+        };
+    };
+
+    return (
+        <>
+        <div className="profile-dropdown">
+            <div>
+                <i className="fas fa-user-circle"/>
+            </div>
+        </div>
+        {showMenu && sessionUser && (
+            <ul className="profile-details">
+                <li>
+                    <div>{sessionUser.firstname}</div>
+                </li>
+                <li>
+                    <NavLink to='/products/new'>Sell on ktsy</NavLink>
+                </li>
+                <li>
+                    <div className="signout-div" onClick={loggingOut}>Sign out</div>
+                </li>
+            </ul>
+        )}
+        </>
+    )
+}
+
+export default ProfileButton;
