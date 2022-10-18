ce9ab0f19db57cf437b0f8127c042123e0585d96
kevin <kmtsou@sbcglobal.net>
2022-10-18 15:18:42 -0700

    adding class names to signup form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -48,69 +48,75 @@ const SignUpForm = ({ setShowSignUpModal }) => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>First Name</label>
-        <input
-          type='text'
-          name='firstname'
-          onChange={(e) => setFirstname(e.target.value)}
-          value={firstname}
-        ></input>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <input
-          type='text'
-          name='lastname'
-          onChange={(e) => setLastname(e.target.value)}
-          value={lastname}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <div className='signup-form-container'>
+      <form onSubmit={onSignUp} className='signup-form'>
+        <div className='signup-header'>
+          <h3 className='signup-header-text'>Create your account</h3>
+          <h4 className='signup-subheader-text'>Registration is easy.</h4>
+        </div>
+        <div className='signup-errors-container'>
+          {errors.map((error, ind) => (
+            <div key={ind} className='signup-errors-line'>{error}</div>
+          ))}
+        </div>
+        <div>
+          <label>User Name</label>
+          <input
+            type='text'
+            name='username'
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type='text'
+            name='email'
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div>
+          <label>First Name</label>
+          <input
+            type='text'
+            name='firstname'
+            onChange={(e) => setFirstname(e.target.value)}
+            value={firstname}
+          ></input>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input
+            type='text'
+            name='lastname'
+            onChange={(e) => setLastname(e.target.value)}
+            value={lastname}
+          ></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div>
+          <label>Repeat Password</label>
+          <input
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <button type='submit'>Sign Up</button>
+      </form>
+    </div>
   );
 };
 
