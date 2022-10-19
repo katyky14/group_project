92d372873b51acf893a8436c5bd1382b596dd63d
TonyTan <tonytan2193@gmail.com>
2022-10-19 15:17:34 -0700

    Added my own review forms, made review modals, made some styling to the homepage

--- react-app/src/components/products/getProducts.js
+++ react-app/src/components/products/getProducts.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { getAllProductsThunk } from "../../store/product";
 import "./homepage.css"
 
@@ -10,14 +10,27 @@ function Products() {
     const history = useHistory();
     const productObj = useSelector(state => state.productState)
     const productArr = Object.values(productObj)
+    const user = useSelector(state => state.session)
 
 
     useEffect(() => {
         dispatch(getAllProductsThunk())
     }, [dispatch])
 
+    const redirect = () => {
+
+        return <Redirect to={{ pathname: 'https://github.com/KatyKy14' }} />
+    }
+
     return !!productArr.length && (
         <div id="homepage">
+            <div id="header-container">
+                <div id="orange-banner"></div>
+                {user && (
+                    <div id="Welcome">Welcome to Buy Ktsy {user.firstname}</div>
+                )}
+                <div id="white-space-div"></div>
+            </div>
             <div id="product-container">
                 {productArr.map(({ id, name, price, images }) => (
                     <div key={id} onClick={() => history.push(`/products/${id}`)} className="homepage-products">
@@ -26,7 +39,18 @@ function Products() {
                     </div>
                 ))}
             </div>
-        </div>
+            <div className="footer">
+                <div id="power-statement">
+                    <p id="statement"><span id="globe">&#127760; </span><span id="text">  Buy Ktsy is powered by Katy, Thenammai, Kevin, and Tony.</span></p>
+                </div>
+                <div id="repos">
+                    <p onClick={redirect}>Katy's Github</p>
+                    <p>https://github.com/thenunachi</p>
+                    <p>https://github.com/kmtsou</p>
+                    <p>https://github.com/TTan321</p>
+                </div>
+            </div>
+        </div >
     )
 
 }
